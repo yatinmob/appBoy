8e507aef942102cd1ca665f7e18870340af69410
Yatin Patel <ypatel@mobiquityinc.com>
2022-04-29 11:22:38 +0530

    Basic Demo Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+declare const AppboyPlugin: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private readonly platform: Platform,
+  ) {
+    this.platform.ready().then(() => {
+      alert(0);
+      debugger;
+      AppboyPlugin.startSessionTracking();
+      AppboyPlugin.changeUser('aa965420-b22a-11e9-84ba-e7271af1e734');
+      AppboyPlugin.setEmail('ypatel@mobiquityinc.com');
+      AppboyPlugin.setFirstName('Yatin');
+      AppboyPlugin.setLastName('Patel');
+    });
+  }
 }
